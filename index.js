cf655a7a6ccf16f6e69ce8cc93ac6f84fb9a2dea
Kevin Isom <kevin.isom@gmail.com>
2015-03-01 17:08:51 +1300

    Tidy up for 1.0.0

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ var path = require('path');
 
 module.exports = function (config) {
 	var env, configFile, baseConfig, overrides;
-	if(arguments.length ===0) {
+	if (arguments.length === 0) {
 		env = process.env.NODE_ENV || 'development';
 	} else {
-		if(typeof config ==='string'){
+		if (typeof config ==='string'){
 			env = config
 		} else { //config is believed to be an object
 			env = config.env ? config.env : process.env.NODE_ENV;
