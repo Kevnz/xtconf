af9cfcfc445d43bfb8da9e734fae320f6a54715c
Kevin Isom <kevin.isom@gmail.com>
2018-03-30 17:20:16 +1300

    formatting and updating

--- index.js
+++ index.js
@@ -2,33 +2,35 @@ var qconf = require('qconf');
 var path = require('path');
 var fs = require('fs');
 
-module.exports = function (config) {
-	var env, configFile, baseConfig, overrides;
-	if (arguments.length === 0) {
-		env = process.env.NODE_ENV || 'development';
-	} else {
-		if (typeof config ==='string'){
-			env = config
-		} else { //config is believed to be an object
-			env = config.env ? config.env : process.env.NODE_ENV;
-			overrides = config;
-			delete overrides.env;
-		} 
-	}
-	configFile = 'file://config/config.' + env + '.json';
-	baseConfig = 'file://config/config.json'; 
-	var configFiles = [baseConfig, configFile];
-	if (env === 'development') {
-		// look for a "named" development file
-		var files = fs.readdirSync('./config');
-		for (var i = 0; i < files.length; i++) {
-			
-			if(files[i].indexOf('config.development.') > -1 && files[i] !== 'config.development.json') {
-				configFiles.push('file://config/'+ files[i]);
-			}
-		};
-	}
-	var c = qconf(overrides, configFiles );
-	return c;
-}
- 
+module.exports = function(config) {
+  var env, configFile, baseConfig, overrides;
+  if (arguments.length === 0) {
+    env = process.env.NODE_ENV || 'development';
+  } else {
+    if (typeof config === 'string') {
+      env = config;
+    } else {
+      //config is believed to be an object
+      env = config.env ? config.env : process.env.NODE_ENV;
+      overrides = config;
+      delete overrides.env;
+    }
+  }
+  configFile = 'file://config/config.' + env + '.json';
+  baseConfig = 'file://config/config.json';
+  var configFiles = [baseConfig, configFile];
+  if (env === 'development') {
+    // look for a "named" development file
+    var files = fs.readdirSync('./config');
+    for (var i = 0; i < files.length; i++) {
+      if (
+        files[i].indexOf('config.development.') > -1 &&
+        files[i] !== 'config.development.json'
+      ) {
+        configFiles.push('file://config/' + files[i]);
+      }
+    }
+  }
+  var c = qconf(overrides, configFiles);
+  return c;
+};
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
 [![Build Status](https://travis-ci.org/Kevnz/xtconf.png?branch=master)](https://travis-ci.org/Kevnz/xtconf)
+
 # xtconf
 
 Extended qconf based on environment
@@ -10,18 +11,17 @@ npm install xtconf --save
 ```
 
 ### Usage
+
 Create a config folder and then json files for each environment like config.development.json, config.production.json
 \config
- -config.json
- -config.development.json
- -config.production.json
- -config.test.json
+-config.json
+-config.development.json
+-config.production.json
+-config.test.json
 
 ```
-
 var config = require('xtconf')();
 config.get('setting');
-
 ```
 
-You can also create a config.json to share settings against multiple environments 
+You can also create a config.json to share settings against multiple environments
